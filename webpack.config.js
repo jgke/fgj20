a8c9ba8abb1fa5f163bee4d6929fe472b1862a7d
Jaakko Hannikainen <jaakko.hannikainen@solita.fi>
2020-02-01 16:42:49 +0200

    Maybe fix livereload?

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,50 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    devtool: 'inline-source-map',
-    devServer: {
-      contentBase: 'public',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpg|bmp)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        emitFile: true
-                    }
-                }]
-            }
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: 'public',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
         ],
-    },
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
-    },
+      },
+      {
+        test: /\.(png|jpg|bmp)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            emitFile: true
+          }
+        }]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  watchOptions: {
+    poll: 1000, // Check for changes every second
+    ignored: /node_modules/
+  }
 };
