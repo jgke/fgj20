b8feba46da1f1a776405792fb8bc1fd0be936fe8
Jaakko Hannikainen <jaakko.hannikainen@solita.fi>
2020-02-01 18:07:36 +0200

    adjust movement

--- src/player.ts
+++ src/player.ts
@@ -2,6 +2,7 @@ import {Actor, CollisionType, Engine, Input, Texture, TileMap, Vector} from 'exc
 
 import {Game} from './.';
 import {tileSize} from "./const";
+import {Direction} from "./Direction";
 
 export class Player extends Actor {
   texture: Texture;
@@ -11,6 +12,7 @@ export class Player extends Actor {
   moveSpeed?: Vector;
   cabling?: number;
   cableOrigin?: Vector;
+  queuedMovement?: Direction;
 
   constructor(
     initPos: Vector,
@@ -30,33 +32,54 @@ export class Player extends Actor {
   update(engine: Game, delta: number) {
     super.update(engine, delta);
 
+    if (engine.input.keyboard.wasPressed(Input.Keys.Up)) {
+      this.queuedMovement = "Up";
+    } else if (engine.input.keyboard.wasPressed(Input.Keys.Down)) {
+      this.queuedMovement = "Down";
+    } else if (engine.input.keyboard.wasPressed(Input.Keys.Left)) {
+      this.queuedMovement = "Left";
+    } else if (engine.input.keyboard.wasPressed(Input.Keys.Right)) {
+      this.queuedMovement = "Right";
+    }
+
     if (this.movingTo) {
-      const distance = this.movingTo.sub(this.pos).scale(0.5);
-      const speed = distance.scale(0.5).add(this.moveSpeed.scale(0.001 * delta));
-      const dOrigin = this.pos.sub(this.movingFrom).magnitude();
-      this.pos = this.pos.add(speed);
-      if (distance.magnitude() < 1 || dOrigin >= 2 * tileSize) {
+      if(this.queuedMovement) {
         this.pos = this.movingTo;
         this.movingTo = undefined;
+      } else {
+        const distance = this.movingTo.sub(this.pos);
+        const speed = distance.scale(0.25).add(this.moveSpeed.scale(0.001 * delta));
+        const dOrigin = this.pos.sub(this.movingFrom).magnitude();
+        this.pos = this.pos.add(speed);
+        if (distance.magnitude() < 1 || dOrigin >= 2 * tileSize) {
+          this.pos = this.movingTo;
+          this.movingTo = undefined;
+        }
       }
     } else {
-      if (engine.input.keyboard.wasPressed(Input.Keys.Up)) {
-        this.movingFrom = this.pos.clone();
-        this.moveSpeed = Vector.Up;
-        this.movingTo = engine.playerMoves(this.pos, "Up");
-      } else if (engine.input.keyboard.wasPressed(Input.Keys.Down)) {
-        this.movingFrom = this.pos.clone();
-        this.moveSpeed = Vector.Down;
-        this.movingTo = engine.playerMoves(this.pos, "Down");
-      } else if (engine.input.keyboard.wasPressed(Input.Keys.Left)) {
-        this.movingFrom = this.pos.clone();
-        this.moveSpeed = Vector.Left;
-        this.movingTo = engine.playerMoves(this.pos, "Left");
-      } else if (engine.input.keyboard.wasPressed(Input.Keys.Right)) {
-        this.movingFrom = this.pos.clone();
-        this.moveSpeed = Vector.Right;
-        this.movingTo = engine.playerMoves(this.pos, "Right");
+      switch (this.queuedMovement) {
+        case "Up":
+          this.movingFrom = this.pos.clone();
+          this.moveSpeed = Vector.Up;
+          this.movingTo = engine.playerMoves(this.pos, "Up");
+          break;
+        case "Down":
+          this.movingFrom = this.pos.clone();
+          this.moveSpeed = Vector.Down;
+          this.movingTo = engine.playerMoves(this.pos, "Down");
+          break;
+        case "Left":
+          this.movingFrom = this.pos.clone();
+          this.moveSpeed = Vector.Left;
+          this.movingTo = engine.playerMoves(this.pos, "Left");
+          break;
+        case "Right":
+          this.movingFrom = this.pos.clone();
+          this.moveSpeed = Vector.Right;
+          this.movingTo = engine.playerMoves(this.pos, "Right");
+          break;
       }
+      this.queuedMovement = undefined;
     }
   }
 }
