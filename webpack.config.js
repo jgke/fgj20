30f0f2eec0d293eb4cb73a18d9284a0725c0e15e
Jaakko Hannikainen <jaakko.hannikainen@solita.fi>
2020-02-01 15:36:41 +0200

    unbreak build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: path.join('src', 'index.ts'),
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
     devtool: 'inline-source-map',
     devServer: {
       contentBase: 'public',
