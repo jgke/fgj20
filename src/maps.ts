dbd6a855d09d80d729f9794bfa8352835cb703a6
Jaakko Hannikainen <jaakko.hannikainen@solita.fi>
2020-02-02 09:55:43 +0200

    maybe level complete stuff

--- src/maps.ts
+++ src/maps.ts
@@ -199,9 +199,9 @@ const EndScreen = [
   [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
 ];
 
-export const maps: {[id: string]: [number[][], string, string?, string?]} = {
-  "00": [helloWorld, "Hello world!", "foobar", "rock"],
-  "01": [introduction, "Introduction", undefined, "player"],
+export const maps: {[id: string]: [number[][], string, string?, string?, string?]} = {
+  "00": [helloWorld, "Hello world!", "foobar", "barfoo", "rock"],
+  "01": [introduction, "Introduction", undefined, undefined, "player"],
   "02": [lifegoals, "Life goals"],
   "03": [orderMatters, "Order matters..."],
   "04": [sokoban1, "Sokoban"],
